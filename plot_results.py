e3c48d4b238d18be75b33a6d216cf296c7676d1e
miri <miritrope@gmail.com>
2020-04-04 16:16:43 +0300

    plotting the whole possible combinations

--- plot_results.py
+++ plot_results.py
@@ -1,27 +1,21 @@
-import pickle as pl
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 # visualize the loss
-def plot_results(exp_name, plotme, batch_size, use_embed_layer, tests_acc):
+def plot_results(exp_name, plotme, batch_size, tests_acc):
     print('Plotting results')
     fig = plt.figure(figsize=(10, 8))
-    lenses = []
-    max_vals = []
 
     for i in range(len(plotme)):
         lab = str(batch_size[i]) + ' '
         lab += f' {tests_acc[i]:.1f}'
         plt.plot(range(1, len(plotme[i]) + 1), plotme[i], label=lab)
-        lenses.append(len(plotme[i]))
-        max_vals.append(np.max(plotme[i]))
 
-    plt.xlabel('epochs')
-    plt.ylabel('valid accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Time')
 
-    plt.xlim(0, 250)
-    plt.ylim(0, 150)
+    plt.xlim(0, 200)
+    plt.ylim(0, 20)
 
     plt.grid(True)
     plt.legend()
@@ -32,8 +26,3 @@ def plot_results(exp_name, plotme, batch_size, use_embed_layer, tests_acc):
     fig.savefig(pic_name, bbox_inches='tight')
     print('Saved picture: ', pic_name)
 
-
-if __name__ == '__main__':
-    file_name = '2020-04-03_valid_acc.pkl'
-    with open(file_name, 'rb') as f:
-        results = pl.load(f)
--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 import learn_model as lm
 import sys
 import os
-import pickle
+import pickle as pl
 from datetime import date
 import plot_results as pr
 
@@ -14,36 +14,57 @@ def enablePrint():
 
 
 batch_sizes = [256, 128, 64, 32]
-n_epochs = 200
-patience = 50
-fold = 1
-use_embed_layer = True
-results = []
 
 
-blockPrint()
-for task in batch_sizes: results.append(lm.execute(fold, task, n_epochs, patience, use_embed_layer))
+def run_tests_train():
+    n_epochs = 200
+    patience = 50
+    fold = 1
+    use_embed_layer = 'True'
 
-enablePrint()
-for r in results: print(r, '\n')
+    blockPrint()
 
-# save the results
-today = date.today()
-if use_embed_layer:
-    experiment = ' with aux net results'
-else:
-    experiment = ' without aux net results'
+    results = []
+    for task in batch_sizes: results.append(lm.execute(fold, task, n_epochs, patience, use_embed_layer))
 
-res_file_name = str(today) + experiment
-with open(res_file_name + '.pkl', 'wb') as f:
-    pickle.dump(results, f)
-print('Saved results data file: ', res_file_name + '.pkl')
+    enablePrint()
+    for r in results: print(r, '\n')
 
-# return [0. train_losses, 1. valid_losses, 2. train_accs, 3. valid_accs, 4. test_acc, 5. epoch_times, 5 train_time]
-train_accs = list(r[2] for r in results)
-valid_accs = list(r[3] for r in results)
-test_accs = list(r[4] for r in results)
+    # save the results
+    today = date.today()
+    if use_embed_layer:
+        experiment = ' with aux net results'
+    else:
+        experiment = ' without aux net results'
+
+    res_file_name = str(today) + experiment
+    with open(res_file_name + '.pkl', 'wb') as f:
+        pl.dump(results, f)
+    print('Saved results data file: ', res_file_name + '.pkl')
+
+    # [0. train_losses, 1. valid_losses, 2. train_accs, 3. valid_accs, 4. test_acc, 5. epoch_times, 6. train_time]
+    train_accs = list(r[2] for r in results)
+    valid_accs = list(r[3] for r in results)
+    test_accs = list(r[4] for r in results)
+
+    # visualize the loss
+    pr.plot_results(experiment, valid_accs, batch_sizes, test_accs)
 
 
-# visualize the loss
-pr.plot_results(experiment, valid_accs, batch_sizes, use_embed_layer, test_accs)
+if __name__ == '__main__':
+    file_name = '2020-04-04 without aux net results.pkl'
+    experiment = 'epoch_times without aux net'
+
+    with open(file_name, 'rb') as f:
+        results = pl.load(f)
+
+    train_accs = list(r[2] for r in results)
+    valid_accs = list(r[3] for r in results)
+    test_accs = list(r[4] for r in results)
+
+    train_losses = list(r[0] for r in results)
+    valid_losses = list(r[1] for r in results)
+    epoch_times = list(r[5] for r in results)
+    train_time = list(r[6] for r in results)
+
+    pr.plot_results(experiment, epoch_times, batch_sizes, train_time)
